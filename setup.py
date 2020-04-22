b666b71b219972dd226b787e231861efa3a4ea09
grburgess <jmichaelburgess@gmail.com>
2020-04-22 20:02:45 +0200

    start up

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+import versioneer
+
+
+# Create list of data files
+def find_data_files(directory):
+
+    paths = []
+
+    for (path, directories, filenames) in os.walk(directory):
+
+        for filename in filenames:
+
+            paths.append(os.path.join("..", path, filename))
+
+    return paths
+
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
